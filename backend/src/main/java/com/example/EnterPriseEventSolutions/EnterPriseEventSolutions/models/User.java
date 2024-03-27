19b041ae152cb898d79c425138bffd60056b6f30
ruky00 <80951317+ruky00@users.noreply.github.com>
2024-03-27 10:45:56 +0100

    ImageService Updated and workflow refactor

--- backend/src/main/java/com/example/EnterPriseEventSolutions/EnterPriseEventSolutions/models/User.java
+++ backend/src/main/java/com/example/EnterPriseEventSolutions/EnterPriseEventSolutions/models/User.java
@@ -183,7 +183,7 @@ public class User {
 
     @Override
     public String toString(){
-        return "User [id=" + id + ", name=" + this.getUsername() + ", email=" + this.getEmail() + "]";
+        return "User [, name=" + this.getUsername() + ", email=" + this.getEmail() + "encodedPassword="+ this.getEncodedPassword()+"]";
     }
 
 
--- backend/src/main/java/com/example/EnterPriseEventSolutions/EnterPriseEventSolutions/services/Image/ImageService.java
+++ backend/src/main/java/com/example/EnterPriseEventSolutions/EnterPriseEventSolutions/services/Image/ImageService.java
@@ -2,9 +2,12 @@ package com.example.EnterPriseEventSolutions.EnterPriseEventSolutions.services.I
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+
 public interface ImageService {
 
-    public String createImage(MultipartFile multiPartFile);
+    public String createImage(MultipartFile imageFile, String imageName, String userName) throws IOException;
 
     public void deleteImage(String image);
 
